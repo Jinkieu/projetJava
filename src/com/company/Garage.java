11639a36b13c6775652dbbd7e4c6718c589c4f25
Jinkieu <quachjulien@gmail.com>
2019-02-11 09:50:04 +0100

    implementation garage

--- src/com/company/Garage.java
+++ src/com/company/Garage.java
@@ -1,4 +1,24 @@
 package com.company;
 
-public class Garage {
+
+import java.util.ArrayList;
+
+public class Garage{
+    ArrayList<Vehicule> listGarage = new ArrayList<Vehicule>();
+
+    public ArrayList getListGarage() {
+        return listGarage;
+    }
+
+    public void setListGarage(ArrayList listGarage) {
+        this.listGarage = listGarage;
+    }
+
+    public Garage() {
+
+    }
+
+    public void addVehicule(Vehicule v1) {
+        listGarage.add(v1);
+    }
 }
